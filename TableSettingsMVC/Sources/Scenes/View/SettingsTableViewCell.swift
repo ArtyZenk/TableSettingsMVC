f632e3974f159c6242addd7bfe7f87fd20379748
Artyom <artyomka13@gmail.com>
2022-04-26 16:37:32 +0300

    delete tabs

--- TableSettingsMVC/Sources/Scenes/View/SettingsTableViewCell.swift
+++ TableSettingsMVC/Sources/Scenes/View/SettingsTableViewCell.swift
@@ -130,24 +130,3 @@ class SettingsTableViewCell: UITableViewCell {
         model.isSwitch ? (contentView.addSubview(switchElement)) : (accessoryType = .disclosureIndicator)
     }
 }
-
-//extension SettingsTableViewCell {
-//    enum Metric {
-//        static let iconContainerCornerRadius: CGFloat = 8
-//        static let labelNumberOfLines = 1
-//        static let additionalLabelNumberOfLines = 1
-//        
-//        static let iconContainerXShift: CGFloat = 15
-//        static let iconContainerYShift: CGFloat = 6
-//        
-//        static let labelXShift: CGFloat = 25
-//        static let labelYShift: CGFloat = 0
-//        static let labelWidthShift: CGFloat = 25
-//        
-//        static let switchElementXShift: CGFloat = 60
-//    }
-//    
-//    enum Strings {
-//        static let identifierViewCell = "SettingsTableViewCell"
-//    }
-//}
