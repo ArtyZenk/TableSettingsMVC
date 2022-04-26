c7dc3e7bd38b43085ccbfc05385567e981125887
Artyom <artyomka13@gmail.com>
2022-04-26 11:38:06 +0300

    add image

--- TableSettingsMVC/Scenes/Model/SettingsModel.swift
+++ TableSettingsMVC/Scenes/Model/SettingsModel.swift
@@ -27,10 +27,10 @@ final class SettingsModel {
             Section(title: "", option: [
                 SettingsCellOption(title: "Авиарежим", additionLabel: nil, icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange, isSwitchButton: true),
                 SettingsCellOption(title: "Wi-Fi", additionLabel: "Не подключено", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemBlue, isSwitchButton: false),
-                SettingsCellOption(title: "Bluetooth", additionLabel: "Вкл.", icon: UIImage(systemName: "iphone.homebutton.radiowaves.left.and.right"), iconBackgroundColor: .systemBlue, isSwitchButton: false),
+                SettingsCellOption(title: "Bluetooth", additionLabel: "Вкл.", icon: UIImage(systemName: "bonjour"), iconBackgroundColor: .systemBlue, isSwitchButton: false),
                 SettingsCellOption(title: "Сотовая связь", additionLabel: nil, icon: UIImage(systemName: "antenna.radiowaves.left.and.right"), iconBackgroundColor: .systemGreen, isSwitchButton: false),
                 SettingsCellOption(title: "Режим модема", additionLabel: nil, icon: UIImage(systemName: "personalhotspot"), iconBackgroundColor: .systemGreen, isSwitchButton: false),
-                SettingsCellOption(title: "VPN", additionLabel: nil, icon: UIImage(systemName: "personalhotspot"), iconBackgroundColor: .systemBlue, isSwitchButton: true)
+                SettingsCellOption(title: "VPN", additionLabel: nil, icon: UIImage(systemName: "network"), iconBackgroundColor: .systemBlue, isSwitchButton: true)
             ]),
             
             Section(title: "", option: [
@@ -42,9 +42,9 @@ final class SettingsModel {
             
             Section(title: "", option: [
                 SettingsCellOption(title: "Основные", additionLabel: nil, icon: UIImage(systemName: "gear"), iconBackgroundColor: .systemGray, isSwitchButton: false),
-                SettingsCellOption(title: "Пункт управления", additionLabel: nil, icon: UIImage(systemName: "house"), iconBackgroundColor: .systemGray, isSwitchButton: false),
+                SettingsCellOption(title: "Пункт управления", additionLabel: nil, icon: UIImage(systemName: "switch.2"), iconBackgroundColor: .systemGray, isSwitchButton: false),
                 SettingsCellOption(title: "Экран и яркость", additionLabel: nil, icon: UIImage(systemName: "textformat.size"), iconBackgroundColor: .systemBlue, isSwitchButton: false),
-                SettingsCellOption(title: "Экран \"Домой\"", additionLabel: nil, icon: UIImage(systemName: "moon.fill"), iconBackgroundColor: .purple, isSwitchButton: false),
+                SettingsCellOption(title: "Экран \"Домой\"", additionLabel: nil, icon: UIImage(systemName: "apps.iphone"), iconBackgroundColor: .purple, isSwitchButton: false),
                 SettingsCellOption(title: "Универсальный доступ", additionLabel: nil, icon: UIImage(systemName: "hourglass"), iconBackgroundColor: .systemBlue, isSwitchButton: false)
             ])
         ]
